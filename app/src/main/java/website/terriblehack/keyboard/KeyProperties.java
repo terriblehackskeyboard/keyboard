19e79a6e54ea1ff9388d537df35940c2070613a5
Feilan Jiang <f-jiang@github.com>
2016-11-26 17:12:11 -0500

    Update to KeyProperties

--- app/src/main/java/website/terriblehack/keyboard/KeyProperties.java
+++ app/src/main/java/website/terriblehack/keyboard/KeyProperties.java
@@ -9,18 +9,26 @@ import java.util.Arrays;
 
 public class KeyProperties {
     private CharSequence label;
-    private int[] code;
+    private int[] codes;
 
     public KeyProperties(CharSequence l, int... c) {
         label = l;
-        code = Arrays.copyOf(c, c.length);
+        codes = Arrays.copyOf(c, c.length);
     }
 
     public int getCode(int i) {
-        return code[i];
+        return codes[i];
     }
 
     public char getLabel(int i) {
         return label.charAt(i);
+    }
+
+    public int[] getCodes() {
+        return Arrays.copyOf(codes, codes.length);
+    }
+
+    public CharSequence getLabels() {
+        return label;
     }
 }
